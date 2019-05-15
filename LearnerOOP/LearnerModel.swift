a50bf01071eff96675a6e73709df1a540dfdcc48
David <kristoforus_david@outlook.com>
2019-05-15 14:19:11 +0700

    Passed CD 10

---
+++ LearnerOOP/LearnerModel.swift
@@ -0,0 +1,33 @@
+//
+//  LearnerModel.swift
+//  LearnerOOP
+//
+//  Created by Kristoforus David on 15/05/19.
+//  Copyright © 2019 Kristoforus David. All rights reserved.
+//
+
+import Foundation
+
+class LearnerModel {
+    var name: String
+    var age: Int
+    var gender: String
+    var imageProfile: String
+    
+    init(nameLearner: String, ageLearner: Int, genderLearner: String, imageProfileLearner: String){
+        self.name = nameLearner
+        self.age = ageLearner
+        self.gender = genderLearner
+        self.imageProfile = imageProfileLearner
+        
+    }
+    
+    func increaseAge(){
+        self.age += 1
+
+        }
+  
+    func decreaseAge(){
+        self.age -= 1
+    }
+}
--- LearnerOOP/ViewController.swift
+++ LearnerOOP/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController{
+
+    @IBOutlet weak var learnerNameLabel: UILabel!
+    
+    @IBOutlet weak var learnerAgeLabel: UILabel!
+    
+    @IBOutlet weak var learnerGenderLabel: UILabel!
+    var learnerInstance:LearnerModel?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let circle = 12
+         learnerInstance = LearnerModel(nameLearner: "David", ageLearner: 24, genderLearner: "Male", imageProfileLearner: "")
+        updateUI()
+    }
+    
+    func updateUI(){
+        if let instance = learnerInstance {
+            learnerNameLabel.text = instance.name
+            learnerAgeLabel.text = "\(instance.age)"
+            learnerGenderLabel.text = instance.gender
+        }
+  
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        if let instance = learnerInstance {
+            instance.increaseAge()
+            updateUI()
+        }
+    }
+    
+    @IBAction func decreaseAgeButton(_ sender: Any) {
+        if let instance = learnerInstance
+        instance.decraseAge()
+        updateUI()
+    }
+    
 }
 
