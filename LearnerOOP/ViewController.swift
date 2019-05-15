f6354ec69e4984a2c8fbf781fb70155e60337b38
David <kristoforus_david@outlook.com>
2019-05-15 12:17:41 +0700

    Initial Commit

---
+++ LearnerOOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnerOOP
+//
+//  Created by Kristoforus David on 15/05/19.
+//  Copyright © 2019 Kristoforus David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
